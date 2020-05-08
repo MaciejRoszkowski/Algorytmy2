5e73ea746fb12dbc377b0ca6b1676e0151247e06
maciej <roszkowskimaiciek@gmail.com>
2020-05-08 19:05:32 +0200

    init

---
+++ Algorithm2/Controllers/PathController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Algorithm2.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PathController : ControllerBase
+    {
+        [HttpGet]
+        public string Gat()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
+            var cords = new Coordinates[400];
+            var weights = new int[400];
+            var lines = System.IO.File.ReadAllLines("test.txt");
+            var style = NumberStyles.AllowExponent | NumberStyles.AllowDecimalPoint | NumberStyles.AllowLeadingSign;
+            for (int i = 0; i < 400; i++)
+            {
+                var line = lines[i + 1].Split(' ');
+                cords[i] = new Coordinates(double.Parse(line[0], style), double.Parse(line[1], style));
+                weights[i] = int.Parse(line[2]);
+            }
+
+            return "asdf";
+        }
+    }
+}
---
+++ Algorithm2/Coordinates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Algorithm2
+{
+    public class Coordinates
+    {
+        public Coordinates()
+        {
+
+        }
+        public Coordinates(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
